9eea930a8f457ecb8fe9315957c34d6d19834436
antonic901 <antonicnikola6@gmail.com>
2023-05-21 12:01:13 +0200

    [e30dbc2] adjust GUIDialogContextMenu: cleaning up initialization/deinitialization of dialog
    - we need additional members to remember window coordinates (these are fetched while loading window .xml file and later are changed when setting position of ContextMenu) and size of background image
    - we don't need to call SetInitialVisibility - it's alreadu done in CGUIWindow::OnWindowInit
    - we need to manually remove choice buttons in OnDeinitWindow
    - we need to store size of background image to properly set its size it in consequent showings

--- xbmc/dialogs/GUIDialogContextMenu.cpp
+++ xbmc/dialogs/GUIDialogContextMenu.cpp
@@ -67,9 +67,12 @@ void CContextButtons::Add(unsigned int button, int label)
   push_back(pair<unsigned int, CStdString>(button, g_localizeStrings.Get(label)));
 }
 
-CGUIDialogContextMenu::CGUIDialogContextMenu(void):CGUIDialog(WINDOW_DIALOG_CONTEXT_MENU, "DialogContextMenu.xml")
+CGUIDialogContextMenu::CGUIDialogContextMenu(void)
+  : CGUIDialog(WINDOW_DIALOG_CONTEXT_MENU, "DialogContextMenu.xml")
 {
   m_clickedButton = -1;
+  m_backgroundImageSize = 0;
+  m_loadType = KEEP_IN_MEMORY;
 }
 
 CGUIDialogContextMenu::~CGUIDialogContextMenu(void)
@@ -164,14 +167,14 @@ void CGUIDialogContextMenu::SetupButtons()
       if (pGroupList->GetOrientation() == VERTICAL)
       {
         // keep gap between bottom edges of grouplist and background image
-        float diff = pControl->GetHeight() - pGroupList->GetHeight();
+        float diff = m_backgroundImageSize - pGroupList->GetHeight();
         pGroupList->SetHeight(pGroupList->GetTotalSize());
         pControl->SetHeight(diff + pGroupList->GetHeight());
       }
       else
       {
         // keep gap between right edges of grouplist and background image
-        float diff = pControl->GetWidth() - pGroupList->GetWidth();
+        float diff = m_backgroundImageSize - pGroupList->GetWidth();
         pGroupList->SetWidth(pGroupList->GetTotalSize());
         pControl->SetWidth(diff + pGroupList->GetWidth());
       }
@@ -640,15 +643,39 @@ CMediaSource *CGUIDialogContextMenu::GetShare(const CStdString &type, const CFil
 
 void CGUIDialogContextMenu::OnWindowLoaded()
 {
+  m_coordX = m_posX;
+  m_coordY = m_posY;
+
+  const CGUIControlGroupList* pGroupList = NULL;
+  const CGUIControl* pControl = GetControl(GROUP_LIST);
+  if (pControl && pControl->GetControlType() == GUICONTROL_GROUPLIST)
+    pGroupList = (CGUIControlGroupList*)pControl;
+
+  pControl = (CGUIControl *)GetControl(BACKGROUND_IMAGE);
+  if (pControl && pGroupList)
+  {
+    if (pGroupList->GetOrientation() == VERTICAL)
+      m_backgroundImageSize = pControl->GetHeight();
+    else
+      m_backgroundImageSize = pControl->GetWidth();
+  }
+
   CGUIDialog::OnWindowLoaded();
-  SetInitialVisibility();
-  SetupButtons();
 }
 
-void CGUIDialogContextMenu::OnWindowUnload()
+void CGUIDialogContextMenu::OnDeinitWindow(int nextWindowID)
 {
+  //we can't be sure that controls are removed on window unload
+  //we have to remove them to be sure that they won't stay for next use of context menu
+  for (unsigned int i = 0; i < m_buttons.size(); i++)
+  {
+    const CGUIControl *control = GetControl(BUTTON_START + i);
+    if (control)
+      RemoveControl(control);
+  }
+
   m_buttons.clear();
-  CGUIDialog::OnWindowUnload();
+  CGUIDialog::OnDeinitWindow(nextWindowID);
 }
 
 CStdString CGUIDialogContextMenu::GetDefaultShareNameByType(const CStdString &strType)
@@ -717,7 +744,7 @@ int CGUIDialogContextMenu::ShowAndGetChoice(const CContextButtons &choices)
   {
     pMenu->m_buttons = choices;
     pMenu->Initialize();
-
+    pMenu->SetupButtons();
     pMenu->PositionAtCurrentFocus();
     pMenu->DoModal();
     return pMenu->m_clickedButton;
@@ -735,7 +762,7 @@ void CGUIDialogContextMenu::PositionAtCurrentFocus()
     {
       CPoint pos = focusedControl->GetRenderPosition() + CPoint(focusedControl->GetWidth() * 0.5f, focusedControl->GetHeight() * 0.5f)
                    + window->GetRenderPosition();
-      SetPosition(m_posX + pos.x - GetWidth() * 0.5f, m_posY + pos.y - GetHeight() * 0.5f);
+      SetPosition(m_coordX + pos.x - GetWidth() * 0.5f, m_coordY + pos.y - GetHeight() * 0.5f);
       return;
     }
   }
