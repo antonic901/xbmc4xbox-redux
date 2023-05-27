ebfe967bbd589b67a149b6081a9e11ad10849ec8
antonic901 <antonicnikola6@gmail.com>
2023-05-27 21:28:31 +0200

    [5359fa2] move lcd.* to the info parser

--- xbmc/GUIInfoManager.cpp
+++ xbmc/GUIInfoManager.cpp
@@ -313,6 +313,26 @@ infomap system_param[] =   {{ "hasalarm",         SYSTEM_HAS_ALARM },
                             { "setting",          SYSTEM_SETTING },
                             { "hasaddon",         SYSTEM_HAS_ADDON }};
 
+infomap lcd_labels[] =     {{ "playicon",         LCD_PLAY_ICON },
+                            { "progressbar",      LCD_PROGRESS_BAR },
+                            { "cputemperature",   LCD_CPU_TEMPERATURE },
+                            { "gputemperature",   LCD_GPU_TEMPERATURE },
+                            { "hddtemperature",   LCD_HDD_TEMPERATURE },
+                            { "fanspeed",         LCD_FAN_SPEED },
+                            { "date",             LCD_DATE },
+                            { "freespace(c)",     LCD_FREE_SPACE_C },
+                            { "freespace(e)",     LCD_FREE_SPACE_E },
+                            { "freespace(f)",     LCD_FREE_SPACE_F },
+                            { "freespace(g)",     LCD_FREE_SPACE_G },
+                            { "time21",           LCD_TIME_21 },
+                            { "time22",           LCD_TIME_22 },
+                            { "timewide21",       LCD_TIME_W21 },
+                            { "timewide22",       LCD_TIME_W22 },
+                            { "time41",           LCD_TIME_41 },
+                            { "time42",           LCD_TIME_42 },
+                            { "time43",           LCD_TIME_43 },
+                            { "time44",           LCD_TIME_44 }};
+
 void CGUIInfoManager::SplitInfoString(const CStdString &infoString, vector< pair<CStdString, CStdString> > &info)
 {
   // our string is of the form:
@@ -430,6 +450,14 @@ int CGUIInfoManager::TranslateSingleString(const CStdString &strCondition)
       {
         if (property == weather[i].str)
           return weather[i].val;
+      }
+    }
+    else if (category == "lcd")
+    {
+      for (size_t i = 0; i < sizeof(lcd_labels) / sizeof(infomap); i++)
+      {
+        if (property == lcd_labels[i].str)
+          return lcd_labels[i].val;
       }
     }
     else if (category == "system")
@@ -562,28 +590,6 @@ int CGUIInfoManager::TranslateSingleString(const CStdString &strCondition)
   {
     CStdString str = strTest.Mid(14, strTest.GetLength() - 15);
     return AddMultiInfo(GUIInfo(WINDOW_PROPERTY, WINDOW_DIALOG_PLUGIN_SETTINGS, ConditionalStringParameter(str)));
-  }
-  else if (strCategory.Equals("lcd"))
-  {
-    if (strTest.Equals("lcd.playicon")) ret = LCD_PLAY_ICON;
-    else if (strTest.Equals("lcd.progressbar")) ret = LCD_PROGRESS_BAR;
-    else if (strTest.Equals("lcd.cputemperature")) ret = LCD_CPU_TEMPERATURE;
-    else if (strTest.Equals("lcd.gputemperature")) ret = LCD_GPU_TEMPERATURE;
-    else if (strTest.Equals("lcd.hddtemperature")) ret = LCD_HDD_TEMPERATURE;
-    else if (strTest.Equals("lcd.fanspeed")) ret = LCD_FAN_SPEED;
-    else if (strTest.Equals("lcd.date")) ret = LCD_DATE;
-    else if (strTest.Equals("lcd.freespace(c)")) ret = LCD_FREE_SPACE_C;
-    else if (strTest.Equals("lcd.freespace(e)")) ret = LCD_FREE_SPACE_E;
-    else if (strTest.Equals("lcd.freespace(f)")) ret = LCD_FREE_SPACE_F;
-    else if (strTest.Equals("lcd.freespace(g)")) ret = LCD_FREE_SPACE_G;
-    else if (strTest.Equals("lcd.Time21")) ret = LCD_TIME_21; // Small LCD numbers
-    else if (strTest.Equals("lcd.Time22")) ret = LCD_TIME_22;
-    else if (strTest.Equals("lcd.TimeWide21")) ret = LCD_TIME_W21; // Medium LCD numbers
-    else if (strTest.Equals("lcd.TimeWide22")) ret = LCD_TIME_W22;
-    else if (strTest.Equals("lcd.Time41")) ret = LCD_TIME_41; // Big LCD numbers
-    else if (strTest.Equals("lcd.Time42")) ret = LCD_TIME_42;
-    else if (strTest.Equals("lcd.Time43")) ret = LCD_TIME_43;
-    else if (strTest.Equals("lcd.Time44")) ret = LCD_TIME_44;
   }
   else if (strCategory.Equals("network"))
   {
