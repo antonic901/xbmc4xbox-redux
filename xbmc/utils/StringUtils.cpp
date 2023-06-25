antonic901/xbmc4xbox-redux

commit 733064afc5ae7837bffc9471d56841633533f164

[3c190a7] Multiple improvements for handling std::vector<std::string>

xbmc/utils/StringUtils.cpp
@@ -102,6 +102,9 @@ int StringUtils::SplitString(const CStdString& input, const CStdString& delimite
 
   results.clear();
 
+  if (input.empty())
+    return 0;
+
   vector<unsigned int> positions;
 
   newPos = input.Find (delimiter, 0);

xbmc/guilib/XMLUtils.cpp
@@ -154,6 +154,30 @@ bool XMLUtils::GetAdditiveString(const TiXmlNode* pRootNode, const char* strTag,
 }
 
 /*!
+  Parses the XML for multiple tags of the given name.
+  Does not clear the array to support chaining.
+*/
+bool XMLUtils::GetStringArray(const TiXmlNode* pRootNode, const char* strTag, std::vector<std::string>& arrayValue, bool clear /* = false */)
+{
+  CStdString strTemp;
+  const TiXmlElement* node = pRootNode->FirstChildElement(strTag);
+  bool bResult=false;
+  if (node && node->FirstChild() && clear)
+    arrayValue.clear();
+  while (node)
+  {
+    if (node->FirstChild())
+    {
+      bResult = true;
+      arrayValue.push_back(node->FirstChild()->Value());
+    }
+    node = node->NextSiblingElement(strTag);
+  }
+
+  return bResult;
+}
+
+/*!
   Returns true if the encoding of the document is other then UTF-8.
   /param strEncoding Returns the encoding of the document. Empty if UTF-8
 */
@@ -212,6 +236,12 @@ void XMLUtils::SetAdditiveString(TiXmlNode* pRootNode, const char *strTag, const
   StringUtils::SplitString(strValue,strSeparator,list);
   for (unsigned int i=0;i<list.size() && !list[i].IsEmpty();++i)
     SetString(pRootNode,strTag,list[i]);
+}
+
+void XMLUtils::SetStringArray(TiXmlNode* pRootNode, const char *strTag, const std::vector<std::string>& arrayValue)
+{
+  for (unsigned int i = 0; i < arrayValue.size(); i++)
+    SetString(pRootNode, strTag, arrayValue.at(i));
 }
 
 void XMLUtils::SetString(TiXmlNode* pRootNode, const char *strTag, const CStdString& strValue)

xbmc/guilib/XMLUtils.h
@@ -42,6 +42,7 @@ public:
   static bool GetBoolean(const TiXmlNode* pRootNode, const char* strTag, bool& bBoolValue);
   static bool GetString(const TiXmlNode* pRootNode, const char* strTag, CStdString& strStringValue);
   static bool GetAdditiveString(const TiXmlNode* pRootNode, const char* strTag, const CStdString& strSeparator, CStdString& strStringValue);
+  static bool GetStringArray(const TiXmlNode* rootNode, const char* tag, std::vector<std::string>& arrayValue, bool clear = false);
   static bool GetEncoding(const TiXmlDocument* pDoc, CStdString& strEncoding);
   static bool GetPath(const TiXmlNode* pRootNode, const char* strTag, CStdString& strStringValue);
   static bool GetFloat(const TiXmlNode* pRootNode, const char* strTag, float& value, const float min, const float max);
@@ -49,6 +50,7 @@ public:
 
   static void SetString(TiXmlNode* pRootNode, const char *strTag, const CStdString& strValue);
   static void SetAdditiveString(TiXmlNode* pRootNode, const char *strTag, const CStdString& strSeparator, const CStdString& strValue);
+  static void SetStringArray(TiXmlNode* pRootNode, const char *strTag, const std::vector<std::string>& arrayValue);
   static void SetInt(TiXmlNode* pRootNode, const char *strTag, int value);
   static void SetFloat(TiXmlNode* pRootNode, const char *strTag, float value);
   static void SetBoolean(TiXmlNode* pRootNode, const char *strTag, bool value);

xbmc/video/VideoDatabase.cpp
@@ -1735,6 +1735,9 @@ CStdString CVideoDatabase::GetValueString(const CVideoInfoTag &details, int min,
     case VIDEODB_TYPE_FLOAT:
       sql += PrepareSQL("c%02d='%f',", i, *(float*)(((char*)&details)+offsets[i].offset));
       break;
+    case VIDEODB_TYPE_STRINGARRAY:
+      sql += PrepareSQL("c%02d='%s',", i, StringUtils::Join(*((std::vector<std::string>*)(((char*)&details)+offsets[i].offset)), g_advancedSettings.m_videoItemSeparator).c_str());
+      break;
     }
   }
   sql.TrimRight(',');
@@ -2698,6 +2701,9 @@ void CVideoDatabase::GetDetailsFromDB(auto_ptr<Dataset> &pDS, int min, int max, 
       break;
     case VIDEODB_TYPE_FLOAT:
       *(float*)(((char*)&details)+offsets[i].offset) = pDS->fv(i+idxOffset).get_asFloat();
+      break;
+    case VIDEODB_TYPE_STRINGARRAY:
+      *(std::vector<std::string>*)(((char*)&details)+offsets[i].offset) = StringUtils::Split(pDS->fv(i+idxOffset).get_asString(), g_advancedSettings.m_videoItemSeparator);
       break;
     }
   }

xbmc/video/VideoDatabase.h
@@ -80,6 +80,7 @@ namespace VIDEO
 #define VIDEODB_TYPE_FLOAT 3
 #define VIDEODB_TYPE_BOOL 4
 #define VIDEODB_TYPE_COUNT 5
+#define VIDEODB_TYPE_STRINGARRAY 6
 
 typedef enum
 {

commit d5e5d5bc603aa19ede2fe46ac647aad8756f735e

[bugfix] opening library (musicdb:// and videodb://) was broken after 733064afc5ae7837bffc9471d56841633533f164

xbmc/utils/StringUtils.cpp
@@ -102,9 +102,6 @@ int StringUtils::SplitString(const CStdString& input, const CStdString& delimite
 
   results.clear();
 
-  if (input.empty())
-    return 0;
-
   vector<unsigned int> positions;
 
   newPos = input.Find (delimiter, 0);