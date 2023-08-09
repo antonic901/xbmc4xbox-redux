62cc7e0ac3839cfef60f48088b870d20713e7bbe
antonic901 <antonicnikola6@gmail.com>
2023-08-09 23:48:02 +0200

    [c91e8e4] musicdb: store a smartplaylist filter as JSON in the musicdb:// URL

--- xbmc/FileSystem/SmartPlaylistDirectory.cpp
+++ xbmc/FileSystem/SmartPlaylistDirectory.cpp
@@ -104,9 +104,16 @@ namespace XFILE
       CMusicDatabase db;
       if (db.Open())
       {
+        CMusicDbUrl musicUrl;
+        CStdString xsp;
+        if (!musicUrl.FromString("musicdb://3/") || !playlist.SaveAsJson(xsp, false))
+          return false;
+
+        // store the smartplaylist as JSON in the URL as well
+        musicUrl.AddOption("xsp", xsp);
+
         CDatabase::Filter filter;
-        filter.where = playlist.GetWhereClause(db, playlists);
-        success = db.GetAlbumsByWhere("musicdb://3/", filter, items, sorting);
+        success = db.GetAlbumsByWhere(musicUrl.ToString(), filter, items, sorting);
         items.SetContent("albums");
         db.Close();
       }
@@ -116,14 +123,20 @@ namespace XFILE
       CMusicDatabase db;
       if (db.Open())
       {
-        CDatabase::Filter filter;
         CSmartPlaylist songPlaylist(playlist);
         if (playlist.GetType().IsEmpty() || playlist.GetType().Equals("mixed"))
           songPlaylist.SetType("songs");
 
-        filter.where = songPlaylist.GetWhereClause(db, playlists);
+        CMusicDbUrl musicUrl;
+        CStdString xsp;
+        if (!musicUrl.FromString("musicdb://4/") || !songPlaylist.SaveAsJson(xsp, false))
+          return false;
 
-        success = db.GetSongsByWhere("", filter, items, sorting);
+        // store the smartplaylist as JSON in the URL as well
+        musicUrl.AddOption("xsp", xsp);
+
+        CDatabase::Filter filter;
+        success = db.GetSongsByWhere(musicUrl.ToString(), filter, items, sorting);
         items.SetContent("songs");
         db.Close();
       }
