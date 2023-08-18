b6cfd3476ed85992fc9dfc06200f6054aa016abd
antonic901 <antonicnikola6@gmail.com>
2023-08-18 23:13:28 +0200

    [99a27e9]  CDirectoryHistory: use a map instead of a vector for m_vecHistory (faster lookups)

--- xbmc/FileSystem/DirectoryHistory.h
+++ xbmc/FileSystem/DirectoryHistory.h
@@ -19,6 +19,8 @@
  *
  */
 
+#include <map>
+
 class CDirectoryHistory
 {
 public:
@@ -30,7 +32,8 @@ public:
     CStdString m_strItem;
     CStdString m_strDirectory;
   };
-  CDirectoryHistory();
+
+  CDirectoryHistory() { }
   virtual ~CDirectoryHistory();
 
   void SetSelectedItem(const CStdString& strSelectedItem, const CStdString& strDirectory);
@@ -44,7 +47,9 @@ public:
   void ClearPathHistory();
   void DumpPathHistory();
 private:
-  std::vector<CHistoryItem> m_vecHistory;
+  static CStdString preparePath(const CStdString &strDirectory, bool tolower = true);
+
+  typedef std::map<CStdString, CHistoryItem> HistoryMap;
+  HistoryMap m_vecHistory;
   std::vector<CStdString> m_vecPathHistory; ///< History of traversed directories
-  CStdString m_strNull;
 };
