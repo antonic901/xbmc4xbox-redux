7457a5f7f5c241c8434139ff551c9263253495ff
antonic901 <antonicnikola6@gmail.com>
2023-09-10 02:44:25 +0200

    [d7e209a] DirectoryCache: The cache keeps track of files and directories and should therefor not be the one to do any substituting.

--- xbmc/FileSystem/DirectoryCache.cpp
+++ xbmc/FileSystem/DirectoryCache.cpp
@@ -67,7 +67,7 @@ bool CDirectoryCache::GetDirectory(const CStdString& strPath, CFileItemList &ite
 {
   CSingleLock lock (m_cs);
 
-  CStdString storedPath = URIUtils::SubstitutePath(strPath);
+  CStdString storedPath = strPath;
   URIUtils::RemoveSlashAtEnd(storedPath);
 
   ciCache i = m_cache.find(storedPath);
@@ -106,7 +106,7 @@ void CDirectoryCache::SetDirectory(const CStdString& strPath, const CFileItemLis
   // this is the best solution for now.
   CSingleLock lock (m_cs);
 
-  CStdString storedPath = URIUtils::SubstitutePath(strPath);
+  CStdString storedPath = strPath;
   URIUtils::RemoveSlashAtEnd(storedPath);
 
   ClearDirectory(storedPath);
@@ -130,7 +130,7 @@ void CDirectoryCache::ClearDirectory(const CStdString& strPath)
 {
   CSingleLock lock (m_cs);
 
-  CStdString storedPath = URIUtils::SubstitutePath(strPath);
+  CStdString storedPath = strPath;
   URIUtils::RemoveSlashAtEnd(storedPath);
 
   iCache i = m_cache.find(storedPath);
@@ -142,7 +142,7 @@ void CDirectoryCache::ClearSubPaths(const CStdString& strPath)
 {
   CSingleLock lock (m_cs);
 
-  CStdString storedPath = URIUtils::SubstitutePath(strPath);
+  CStdString storedPath = strPath;
   URIUtils::RemoveSlashAtEnd(storedPath);
 
   iCache i = m_cache.begin();
@@ -179,11 +179,11 @@ bool CDirectoryCache::FileExists(const CStdString& strFile, bool& bInCache)
   CSingleLock lock (m_cs);
   bInCache = false;
 
-  CStdString strPath;
-  URIUtils::GetDirectory(strFile, strPath);
-  URIUtils::RemoveSlashAtEnd(strPath);
+  CStdString storedPath;
+  URIUtils::GetDirectory(strFile, storedPath);
+  URIUtils::RemoveSlashAtEnd(storedPath);
 
-  ciCache i = m_cache.find(strPath);
+  ciCache i = m_cache.find(storedPath);
   if (i != m_cache.end())
   {
     bInCache = true;
