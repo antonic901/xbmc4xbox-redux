c7c1e80d4e0bd5e792c804cb49545764c4bfa676
antonic901 <antonicnikola6@gmail.com>
2023-10-28 12:49:25 +0200

    [screensavers] added: Drempels screensaver addon

--- sources/screensavers/Drempels/Drempels.cpp
+++ sources/screensavers/Drempels/Drempels.cpp
@@ -1,13 +1,9 @@
 
 #include <xtl.h>
+#include "../../../xbmc/addons/include/xbmc_scr_dll.h"
+#include "../../../xbmc/addons/include/xbmc_addon_cpp_dll.h"
 
 #pragma comment (lib, "lib/xbox_dx8.lib")
-
-
-struct SCR_INFO 
-{
-	int	dummy;
-};
 
 extern long FXW;
 extern long FXH;
@@ -22,14 +18,20 @@ void DrempelsExit();
 //-- Create -------------------------------------------------------------------
 //
 //-----------------------------------------------------------------------------
-extern "C" void Create(LPDIRECT3DDEVICE8 pd3dDevice, int iWidth, int iHeight, const char* szScreenSaverName)
+extern "C" ADDON_STATUS Create(void* hdl, void* props)
 {
+  if (!props)
+    return STATUS_UNKNOWN;
+
+  SCR_PROPS* scrprops = (SCR_PROPS*)props;
+
   FXW = 512;
   FXH = 512;
-  g_width = iWidth;
-  g_height = iHeight;
-  g_pd3dDevice = pd3dDevice;
+  g_width = scrprops->width;
+  g_height = scrprops->height;
+  g_pd3dDevice = (LPDIRECT3DDEVICE8)scrprops->device;
 
+  return STATUS_OK;
 } // Create
 
 //-- Start --------------------------------------------------------------------
@@ -60,6 +62,56 @@ extern "C" void Stop()
 
 } // Stop
 
+//-- Destroy-------------------------------------------------------------------
+// Do everything before unload of this add-on
+// !!! Add-on master function !!!
+//-----------------------------------------------------------------------------
+extern "C" void Destroy()
+{
+}
+
+//-- HasSettings --------------------------------------------------------------
+// Returns true if this add-on use settings
+// !!! Add-on master function !!!
+//-----------------------------------------------------------------------------
+extern "C" bool HasSettings()
+{
+  return false;
+}
+
+//-- GetStatus ---------------------------------------------------------------
+// Returns the current Status of this visualisation
+// !!! Add-on master function !!!
+//-----------------------------------------------------------------------------
+extern "C" ADDON_STATUS GetStatus()
+{
+  return STATUS_OK;
+}
+
+//-- GetSettings --------------------------------------------------------------
+// Return the settings for XBMC to display
+//-----------------------------------------------------------------------------
+
+extern "C" unsigned int GetSettings(StructSetting ***sSet)
+{
+  return 0;
+}
+
+//-- FreeSettings --------------------------------------------------------------
+// Free the settings struct passed from XBMC
+//-----------------------------------------------------------------------------
+extern "C" void FreeSettings()
+{
+}
+
+//-- UpdateSetting ------------------------------------------------------------
+// Handle setting change request from XBMC
+//-----------------------------------------------------------------------------
+extern "C" ADDON_STATUS SetSetting(const char* id, const void* value)
+{
+  return STATUS_UNKNOWN;
+}
+
 //-- GetInfo ------------------------------------------------------------------
 //
 //-----------------------------------------------------------------------------
@@ -69,27 +121,3 @@ extern "C" void GetInfo(SCR_INFO* pInfo)
 	// back to XBMC if required in the future
 	return;
 }
-
-extern "C" 
-{
-
-	struct ScreenSaver
-	{
-	public:
-		void (__cdecl* Create)(LPDIRECT3DDEVICE8 pd3dDevice, int iWidth, int iHeight, const char* szScreensaver);
-		void (__cdecl* Start) ();
-		void (__cdecl* Render) ();
-		void (__cdecl* Stop) ();
-		void (__cdecl* GetInfo)(SCR_INFO *info);
-	} ;
-
-
-	void __declspec(dllexport) get_module(struct ScreenSaver* pScr)
-	{
-		pScr->Create = Create;
-		pScr->Start = Start;
-		pScr->Render = Render;
-		pScr->Stop = Stop;
-		pScr->GetInfo = GetInfo;
-	}
-};
