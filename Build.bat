5f87dcd2aa3d4301c61b53745b34ffc09d7c92da
antonic901 <antonicnikola6@gmail.com>
2023-11-05 22:01:58 +0100

    [visualizations] removed: no longer needed visualizations binaries

--- Build.bat
+++ Build.bat
@@ -229,9 +229,7 @@ GOTO:EOF
 
   
   xcopy addons %~1\addons /E /Q /I /Y /EXCLUDE:exclude.txt
-  rem xcopy credits %~1\credits /Q /I /Y /EXCLUDE:exclude.txt
   xcopy language %~1\language /E /Q /I /Y /EXCLUDE:exclude.txt
-  xcopy visualisations %~1\visualisations /E /Q /I /Y /EXCLUDE:exclude.txt
   xcopy system %~1\system /E /Q /I /Y /EXCLUDE:exclude.txt
   xcopy web\XBMC_Reloaded %~1\web /E /Q /I /Y /EXCLUDE:exclude.txt
   xcopy media   %~1\media   /E /Q /I /Y /EXCLUDE:exclude.txt
