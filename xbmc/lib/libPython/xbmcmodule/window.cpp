a800abf03bb2c4396a832a3d5bc01626f826e7d5
antonic901 <antonicnikola6@gmail.com>
2023-11-14 19:32:18 +0100

    [Python] fixed: must reset callback window when python window object goes away - based on 8478fb2

--- xbmc/lib/libPython/xbmcmodule/window.cpp
+++ xbmc/lib/libPython/xbmcmodule/window.cpp
@@ -363,6 +363,8 @@ namespace PYXBMC
         // old window does not exist anymore, switch to home
         else g_windowManager.ActivateWindow(WINDOW_HOME);
       }
+      // no callbacks are possible any longer
+      ((CGUIPythonWindowXML*)self->pWindow)->SetCallbackWindow(NULL);
     }
     else
     {
