2bf50884d4c38b4da23dc9e3841aa0c8300a07bc
antonic901 <antonicnikola6@gmail.com>
2023-11-14 01:09:33 +0100

    [addons] [fix] - make changed settings visible to python addons - based on 0c2e7a9

--- xbmc/addons/Addon.cpp
+++ xbmc/addons/Addon.cpp
@@ -404,9 +404,9 @@ bool CAddon::HasSettings()
   return LoadSettings();
 }
 
-bool CAddon::LoadSettings()
+bool CAddon::LoadSettings(bool bForce /* = false*/)
 {
-  if (m_settingsLoaded)
+  if (m_settingsLoaded && !bForce)
     return true;
   if (!m_hasSettings)
     return false;
@@ -441,6 +441,11 @@ bool CAddon::HasUserSettings()
   return m_userSettingsLoaded;
 }
 
+bool CAddon::ReloadSettings()
+{
+  return LoadSettings(true);
+}
+
 bool CAddon::LoadUserSettings()
 {
   m_userSettingsLoaded = false;
@@ -472,6 +477,8 @@ void CAddon::SaveSettings(void)
   TiXmlDocument doc;
   SettingsToXML(doc);
   doc.SaveFile(m_userSettingsPath);
+
+  CAddonMgr::Get().ReloadSettings(ID());//push the settings changes to the running addon instance
 }
 
 CStdString CAddon::GetSetting(const CStdString& key)
