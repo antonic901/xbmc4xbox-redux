2f692fabd0430fde7fee7a4ac5b6c4e6a7952781
antonic901 <antonicnikola6@gmail.com>
2024-01-14 21:41:11 +0100

    [815cf23] settings: move logic to load playercorefactory.xml into CPlayerCoreFactory

--- xbmc/settings/Settings.cpp
+++ xbmc/settings/Settings.cpp
@@ -769,8 +769,8 @@ bool CSettings::LoadSettings(const CStdString& strSettingsFile)
   LoadSkinSettings(pRootElement);
 
   // Configure the PlayerCoreFactory
-  LoadPlayerCoreFactorySettings("special://xbmc/system/playercorefactory.xml", true);
-  LoadPlayerCoreFactorySettings(GetUserDataItem("playercorefactory.xml"), false);
+  CPlayerCoreFactory::Get().LoadConfiguration("special://xbmc/system/playercorefactory.xml", true);
+  CPlayerCoreFactory::Get().LoadConfiguration(GetUserDataItem("playercorefactory.xml"), false);
 
   // Advanced settings
   g_advancedSettings.Load();
@@ -797,25 +797,6 @@ bool CSettings::LoadSettings(const CStdString& strSettingsFile)
     CLog::Log(LOGNOTICE, "Per AV pack settings are off");
 
   return true;
-}
-
-bool CSettings::LoadPlayerCoreFactorySettings(const CStdString& fileStr, bool clear)
-{
-  CLog::Log(LOGNOTICE, "Loading player core factory settings from %s.", fileStr.c_str());
-  if (!CFile::Exists(fileStr))
-  { // tell the user it doesn't exist
-    CLog::Log(LOGNOTICE, "%s does not exist. Skipping.", fileStr.c_str());
-    return false;
-  }
-
-  TiXmlDocument playerCoreFactoryXML;
-  if (!playerCoreFactoryXML.LoadFile(fileStr))
-  {
-    CLog::Log(LOGERROR, "Error loading %s, Line %d (%s)", fileStr.c_str(), playerCoreFactoryXML.ErrorRow(), playerCoreFactoryXML.ErrorDesc());
-    return false;
-  }
-
-  return CPlayerCoreFactory::Get().LoadConfiguration(playerCoreFactoryXML.RootElement(), clear);
 }
 
 bool CSettings::LoadAvpackXML()
