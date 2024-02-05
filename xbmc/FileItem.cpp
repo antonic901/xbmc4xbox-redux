531963d0b70324c86e752adbfd3958baeb7193bb
antonic901 <antonicnikola6@gmail.com>
2024-02-05 17:49:53 +0100

    [082152c] lowercase video_ts folders didn't work for GetLocalMetadataPath

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -2893,7 +2893,7 @@ CStdString CFileItem::GetLocalMetadataPath() const
   CStdString parentFolder(parent);
   URIUtils::RemoveSlashAtEnd(parentFolder);
   parentFolder = URIUtils::GetFileName(parentFolder);
-  if (parentFolder == "VIDEO_TS" || parentFolder == "BDMV")
+  if (parentFolder.CompareNoCase("VIDEO_TS") == 0 || parentFolder.CompareNoCase("BDMV") == 0)
   { // go back up another one
     parent = URIUtils::GetParentPath(parent);
   }
