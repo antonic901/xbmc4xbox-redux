3a2e582a83e189f96d9d0115a2eaccad97298138
antonic901 <antonicnikola6@gmail.com>
2024-02-27 00:12:33 +0100

    [47fc866] Fixed an issue where Events that were supposed to have been initialized as 'signaled' weren't. Added the ability to intialize an event as 'signaled' and added the appropriate unit tests.

--- xbmc/threads/Thread.cpp
+++ xbmc/threads/Thread.cpp
@@ -51,7 +51,7 @@ static XbmcThreads::ThreadLocal<CThread> currentThread;
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-CThread::CThread(const char* ThreadName) : m_StopEvent(true)
+CThread::CThread(const char* ThreadName) : m_StopEvent(true,true)
 {
   m_bStop = false;
 
@@ -68,7 +68,7 @@ CThread::CThread(const char* ThreadName) : m_StopEvent(true)
     m_ThreadName = ThreadName;
 }
 
-CThread::CThread(IRunnable* pRunnable, const char* ThreadName) : m_StopEvent(true)
+CThread::CThread(IRunnable* pRunnable, const char* ThreadName) : m_StopEvent(true,true)
 {
   m_bStop = false;
 
