3fc156d63356fadce3d736d00a963a45b4ddefd2
antonic901 <antonicnikola6@gmail.com>
2024-03-11 15:22:53 +0100

    [ea50355] [WIN32] fixed: python path needs string charset and not utf8. This should fix the import error on paths with special chars (dunno if this is the right place to fix but I didn't found a better one. maybe we can convert the addon path returned by LibPath() when storing it but I dunno if this has other side effects).

--- lib/libPython/XBPyThread.cpp
+++ lib/libPython/XBPyThread.cpp
@@ -166,7 +166,15 @@ void XBPyThread::Process()
   ADDON::VECADDONS addons;
   ADDON::CAddonMgr::Get().GetAddons(ADDON::ADDON_SCRIPT_MODULE, addons);
   for (unsigned int i = 0; i < addons.size(); ++i)
+#ifdef TARGET_WINDOWS
+  {
+    CStdString strTmp(CSpecialProtocol::TranslatePath(addons[i]->LibPath()));
+    g_charsetConverter.utf8ToStringCharset(strTmp);
+    path += PY_PATH_SEP + strTmp;
+  }
+#else
     path += PY_PATH_SEP + CSpecialProtocol::TranslatePath(addons[i]->LibPath());
+#endif
 
   // and add on whatever our default path is
   path += PY_PATH_SEP;
