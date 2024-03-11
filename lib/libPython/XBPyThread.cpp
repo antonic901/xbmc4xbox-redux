9c40a936d940db93d765c3ceb838bff4f5adc152
antonic901 <antonicnikola6@gmail.com>
2024-03-11 15:26:14 +0100

    [ba7c6fa] [fix] xbmc.Monitor onAbortRequested() hanging on exit

--- lib/libPython/XBPyThread.cpp
+++ lib/libPython/XBPyThread.cpp
@@ -376,9 +376,6 @@ void XBPyThread::Process()
     Py_XDECREF(pystring);
   }
 
-  //tell xbmc.Monitor to call onAbortRequested()    
-  g_pythonParser.OnAbortRequested();
-
   PyObject *m = PyImport_AddModule((char*)"xbmc");
   if(!m || PyObject_SetAttrString(m, (char*)"abortRequested", PyBool_FromLong(1)))
     CLog::Log(LOGERROR, "Scriptresult: failed to set abortRequested");
@@ -466,7 +463,7 @@ void XBPyThread::stop()
     PyThreadState* old = PyThreadState_Swap((PyThreadState*)m_threadState);
 
     //tell xbmc.Monitor to call onAbortRequested()
-    g_pythonParser.OnAbortRequested();
+    g_pythonParser.OnAbortRequested(addon->ID());
 
     PyObject *m;
     m = PyImport_AddModule((char*)"xbmc");
