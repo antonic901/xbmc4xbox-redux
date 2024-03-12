d7cb05be5f2788d486af264e9ee3e3f3f525b20e
antonic901 <antonicnikola6@gmail.com>
2024-03-12 22:45:59 +0100

    [5f561f7] fixed: File_Type needs a Py_INCREF before PyModule_AddObject

--- lib/libPython/xbmcmodule/xbmcvfsmodule.cpp
+++ lib/libPython/xbmcmodule/xbmcvfsmodule.cpp
@@ -575,6 +575,7 @@ extern "C" {
       pXbmcvfsModule = Py_InitModule((char*)"xbmcvfs", xbmcvfsMethods);
       if (pXbmcvfsModule == NULL) return;
 
+      Py_INCREF(&File_Type);
       PyModule_AddObject(pXbmcvfsModule, (char*)"File", (PyObject*)&File_Type);
 
       // constants
