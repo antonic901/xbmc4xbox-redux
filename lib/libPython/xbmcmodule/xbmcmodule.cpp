c7c2d89ab0386f5b4b0ff31d5d2ac94b5a6eb27e
antonic901 <antonicnikola6@gmail.com>
2024-03-12 21:17:31 +0100

    [879be40] [fixed] use of a char* after the originating std::string goes out of scope.

--- lib/libPython/xbmcmodule/xbmcmodule.cpp
+++ lib/libPython/xbmcmodule/xbmcmodule.cpp
@@ -492,7 +492,7 @@ namespace PYXBMC
 
   PyObject* XBMC_GetInfoLabel(PyObject *self, PyObject *args)
   {
-    const char* cret;
+    std::string cret;
 
     char *cLine = NULL;
     if (!PyArg_ParseTuple(args, (char*)"s", &cLine)) return NULL;
@@ -501,9 +501,9 @@ namespace PYXBMC
       CPyThreadState gilRelease;
 
       int ret = g_infoManager.TranslateString(cLine);
-      cret = g_infoManager.GetLabel(ret).c_str();
+      cret = g_infoManager.GetLabel(ret);
     }
-    return Py_BuildValue((char*)"s", cret);
+    return Py_BuildValue((char*)"s", cret.c_str());
   }
 
   // getInfoImage() method
@@ -520,7 +520,7 @@ namespace PYXBMC
 
   PyObject* XBMC_GetInfoImage(PyObject *self, PyObject *args)
   {
-    const char* cret;
+    std::string cret;
 
     char *cLine = NULL;
     if (!PyArg_ParseTuple(args, (char*)"s", &cLine)) return NULL;
@@ -529,10 +529,10 @@ namespace PYXBMC
       CPyThreadState gilRelease;
 
       int ret = g_infoManager.TranslateString(cLine);
-      cret = g_infoManager.GetImage(ret, WINDOW_INVALID).c_str();
+      cret = g_infoManager.GetImage(ret, WINDOW_INVALID);
     }
 
-    return Py_BuildValue((char*)"s", cret);
+    return Py_BuildValue((char*)"s", cret.c_str());
   }
 
   // playSFX() method
