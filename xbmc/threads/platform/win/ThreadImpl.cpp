670a8a38d7911abd0025da5f1c6fbf3e8c951c59
antonic901 <antonicnikola6@gmail.com>
2024-03-15 16:37:17 +0100

    [25c1e77] [WIN32] fixed: GetCurrentThread() can return NULL and GetThreadPriority() return THREAD_PRIORITY_ERROR_RETURN which cause GetThreadPriority() and SetThreadPriority() called with an invalid HANDLE or priority.

--- xbmc/threads/platform/win/ThreadImpl.cpp
+++ xbmc/threads/platform/win/ThreadImpl.cpp
@@ -140,8 +140,9 @@ bool CThread::WaitForThreadExit(unsigned int milliseconds)
   {
     // boost priority of thread we are waiting on to same as caller
     int callee = GetThreadPriority(m_ThreadOpaque.handle);
-    int caller = GetThreadPriority(GetCurrentThread());
-    if(caller > callee)
+    HANDLE cthread = GetCurrentThread();
+    int caller = (cthread != NULL) ? GetThreadPriority(cthread) : 0;
+    if(cthread != NULL && caller != THREAD_PRIORITY_ERROR_RETURN && caller > callee)
       SetThreadPriority(m_ThreadOpaque.handle, caller);
 
     lock.Leave();
@@ -149,7 +150,7 @@ bool CThread::WaitForThreadExit(unsigned int milliseconds)
     lock.Enter();
 
     // restore thread priority if thread hasn't exited
-    if(caller > callee && m_ThreadOpaque.handle)
+    if(callee != THREAD_PRIORITY_ERROR_RETURN && caller > callee && m_ThreadOpaque.handle)
       SetThreadPriority(m_ThreadOpaque.handle, callee);
   }
   return bReturn;
