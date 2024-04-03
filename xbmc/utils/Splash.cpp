c71319631286e0a35ba531f2617ac0a00b9c1083
antonic901 <antonicnikola6@gmail.com>
2024-04-04 00:59:36 +0200

    [8c7f7e8] fixed: splash screen could crash xbmc and X server
    We can't use gfx rutines before BeginRender() call
    since the opengl context is not setup properly before
    that.

--- xbmc/utils/Splash.cpp
+++ xbmc/utils/Splash.cpp
@@ -56,13 +56,13 @@ void CSplash::Show()
   g_graphicsContext.SetRenderingResolution(res, true);
   CGUIImage* image = new CGUIImage(0, 0, 0, 0, 1280, 720, m_ImageName);
   image->SetAspectRatio(CAspectRatio::AR_CENTER);
-  image->AllocResources();
 
   //render splash image
 #ifndef HAS_XBOX_D3D
   g_graphicsContext.Get3DDevice()->BeginScene();
 #endif
 
+  image->AllocResources();
   image->Render();
   image->FreeResources();
   delete image;
