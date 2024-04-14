21e5dcfc884c987b15a7e9929bd64d280c2d7b01
antonic901 <antonicnikola6@gmail.com>
2024-04-14 22:24:32 +0200

    [2683aef] use the cached version of textures for JSON-RPC

--- xbmc/video/VideoInfoTag.cpp
+++ xbmc/video/VideoInfoTag.cpp
@@ -27,7 +27,7 @@
 #include "utils/Variant.h"
 #include "utils/Archive.h"
 #include "utils/CharsetConverter.h"
-#include "ThumbnailCache.h"
+#include "TextureCache.h"
 #include "filesystem/File.h"
 
 #include <sstream>
@@ -426,8 +426,8 @@ void CVideoInfoTag::Serialize(CVariant& value)
     CVariant actor;
     actor["name"] = m_cast[i].strName;
     actor["role"] = m_cast[i].strRole;
-    if (!m_cast[i].thumb.IsEmpty())
-      actor["thumbnail"] = m_cast[i].thumb;
+    if (!m_cast[i].thumb.IsEmpty()) // TODO: json-rpc should use real URLs just like everywhere else
+      actor["thumbnail"] = CTextureCache::Get().CheckAndCacheImage(m_cast[i].thumb);
     value["cast"].push_back(actor);
   }
   value["set"] = m_strSet;
