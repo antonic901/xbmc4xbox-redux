3bc3d005263d14658beb7807ab925724c401e502
antonic901 <antonicnikola6@gmail.com>
2024-04-18 11:01:38 +0200

    [e079b96] [thumbs] deprecate HTTP-API GetThumbFilename for albums

--- lib/libGoAhead/XBMChttp.cpp
+++ lib/libGoAhead/XBMChttp.cpp
@@ -57,7 +57,6 @@
 #include "utils/URIUtils.h"
 #include "utils/log.h"
 #include "TextureCache.h"
-#include "ThumbnailCache.h"
 #include "utils/SystemInfo.h"
 #include "ThumbLoader.h"
 
@@ -1886,15 +1885,7 @@ int CXbmcHttp::xbmcGetThumb(int numParas, CStdString paras[], bool bGetThumb)
 
 int CXbmcHttp::xbmcGetThumbFilename(int numParas, CStdString paras[])
 {
-  CStdString thumbFilename="";
-
-  if (numParas>1)
-  {
-    thumbFilename = CThumbnailCache::GetAlbumThumb(paras[0], paras[1]);
-    return SetResponse(openTag+thumbFilename ) ;
-  }
-  else
-    return SetResponse(openTag+"Error:Missing parameter (album;artist)") ;
+  return SetResponse(openTag+"Error:Deprecated function") ;
 }
 
 int CXbmcHttp::xbmcPlayerPlayFile(int numParas, CStdString paras[])
