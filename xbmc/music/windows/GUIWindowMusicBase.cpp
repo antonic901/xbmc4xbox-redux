f2b9acb332d14c3191ebdedfdd188dc0387e237f
antonic901 <antonicnikola6@gmail.com>
2024-04-18 09:29:07 +0200

    [9f08d93] [infoscanner] add CategoriseAlbums to MusicInfoScanner as an improved version of CheckForVariousArtists

--- xbmc/music/windows/GUIWindowMusicBase.cpp
+++ xbmc/music/windows/GUIWindowMusicBase.cpp
@@ -1217,9 +1217,9 @@ void CGUIWindowMusicBase::UpdateThumb(const CAlbum &album, const CStdString &pat
         songs.push_back(song);
       }
     }
-    CMusicInfoScanner::CheckForVariousArtists(songs);
-    CStdString album, artist;
-    if (CMusicInfoScanner::HasSingleAlbum(songs, album, artist))
+    VECALBUMS albums;
+    CMusicInfoScanner::CategoriseAlbums(songs, albums);
+    if (albums.size() == 1)
     { // can cache as the folder thumb
       CStdString folderThumb(CThumbnailCache::GetMusicThumb(albumPath));
       CFile::Copy(albumThumb, folderThumb);
