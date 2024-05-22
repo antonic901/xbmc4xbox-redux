a4ff49a7ec41ec36ba2476a59e063ffec64267cc
antonic901 <antonicnikola6@gmail.com>
2024-05-22 10:33:59 +0200

    [7c515b8] Always mark GUISliderControl for now

--- xbmc/guilib/GUISliderControl.h
+++ xbmc/guilib/GUISliderControl.h
@@ -60,6 +60,7 @@ public:
   virtual ~CGUISliderControl(void);
   virtual CGUISliderControl *Clone() const { return new CGUISliderControl(*this); };
 
+  virtual void Process(unsigned int currentTime);
   virtual void Render();
   virtual bool OnAction(const CAction &action);
   virtual void PreAllocResources();
