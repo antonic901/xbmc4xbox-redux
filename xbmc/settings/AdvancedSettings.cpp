antonic901/xbmc4xbox-redux

commit 339346c39d60eb892dba921448611234d2f0f235

[a4544cb] Add busydialogdelayms advanced setting.
Provides control over the delay until busy dialog is shown when starting
video playback.

xbmc/settings/AdvancedSettings.cpp
@@ -138,6 +138,8 @@ void CAdvancedSettings::Initialize()
   m_videoIgnorePercentAtEnd   = 8.0f;
   m_videoPlayCountMinimumPercent = 90.0f;
 
+  m_videoBusyDialogDelay_ms = 100;
+
   m_musicUseTimeSeeking = true;
   m_musicTimeSeekForward = 10;
   m_musicTimeSeekBackward = -10;
@@ -439,6 +441,10 @@ void CAdvancedSettings::ParseSettingsFile(const CStdString &file)
     XMLUtils::GetString(pElement,"ppffmpegdeinterlacing",m_videoPPFFmpegDeint);
     XMLUtils::GetString(pElement,"ppffmpegpostprocessing",m_videoPPFFmpegPostProc);
   }
+
+  // controls the delay, in milliseconds, until
+  // the busy dialog is shown when starting video playback.
+  XMLUtils::GetInt(pElement, "busydialogdelayms", m_videoBusyDialogDelay_ms, 0, 1000);
 
   pElement = pRootElement->FirstChildElement("musiclibrary");
   if (pElement)

xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -405,13 +405,16 @@ bool CDVDPlayer::OpenFile(const CFileItem& file, const CPlayerOptions &options)
 
     m_ready.Reset();
     Create();
-    if(!m_ready.WaitMSec(100))
+    if(!m_ready.WaitMSec(g_advancedSettings.m_videoBusyDialogDelay_ms))
     {
       CGUIDialogBusy* dialog = (CGUIDialogBusy*)g_windowManager.GetWindow(WINDOW_DIALOG_BUSY);
-      dialog->Show();
-      while(!m_ready.WaitMSec(1))
-        g_windowManager.ProcessRenderLoop(true);
-      dialog->Close();
+      if(dialog)
+      {
+        dialog->Show();
+        while(!m_ready.WaitMSec(1))
+          g_windowManager.ProcessRenderLoop(true);
+        dialog->Close();
+      }
     }
 
     // Playback might have been stopped due to some error

xbmc/settings/AdvancedSettings.h
@@ -122,6 +122,8 @@ class CAdvancedSettings : public ISettingCallback, public ISettingsHandler
     float m_videoIgnorePercentAtEnd;
     bool m_audioApplyDrc;
 
+    int  m_videoBusyDialogDelay_ms;
+
     float m_videoPlayCountMinimumPercent;
     
     bool m_guiKeepInMemory;

commit 0cc6bbdeb53f86ce556aeaa224ae00a0ebdb3a05

fixed: loading advanced settings was broken after 339346c39d60eb892dba921448611234d2f0f235

xbmc/settings/AdvancedSettings.cpp
@@ -442,11 +442,11 @@ void CAdvancedSettings::ParseSettingsFile(const CStdString &file)
     XMLUtils::GetString(pElement,"cleandatetime", m_videoCleanDateTimeRegExp);
     XMLUtils::GetString(pElement,"ppffmpegdeinterlacing",m_videoPPFFmpegDeint);
     XMLUtils::GetString(pElement,"ppffmpegpostprocessing",m_videoPPFFmpegPostProc);
-  }
 
-  // controls the delay, in milliseconds, until
-  // the busy dialog is shown when starting video playback.
-  XMLUtils::GetInt(pElement, "busydialogdelayms", m_videoBusyDialogDelay_ms, 0, 1000);
+    // controls the delay, in milliseconds, until
+    // the busy dialog is shown when starting video playback.
+    XMLUtils::GetInt(pElement, "busydialogdelayms", m_videoBusyDialogDelay_ms, 0, 1000);
+  }
 
   pElement = pRootElement->FirstChildElement("musiclibrary");
   if (pElement)