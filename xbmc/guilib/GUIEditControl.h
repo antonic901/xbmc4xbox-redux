337d0c384383d93724698da2cda534f889df29de
antonic901 <antonicnikola6@gmail.com>
2024-05-31 00:17:39 +0200

    [CGUIEditControl] updated to match Kodi Krypton

--- xbmc/guilib/GUIEditControl.h
+++ xbmc/guilib/GUIEditControl.h
@@ -31,6 +31,7 @@
 #include "GUIButtonControl.h"
 #include "utils/Stopwatch.h"
 #include "utils/StringValidation.h"
+#include "utils/Variant.h"
 
 /*!
  \ingroup controls
@@ -75,56 +76,69 @@ public:
   unsigned int GetCursorPosition() const;
   void SetCursorPosition(unsigned int iPosition);
 
-  void SetInputType(INPUT_TYPE type, int heading);
-  
+  void SetInputType(INPUT_TYPE type, CVariant heading);
+
   void SetTextChangeActions(const CGUIAction& textChangeActions) { m_textChangeActions = textChangeActions; };
-  
-  bool HasTextChangeActions() { return m_textChangeActions.HasActionsMeetingCondition(); };
+
+  bool HasTextChangeActions() const { return m_textChangeActions.HasActionsMeetingCondition(); };
 
   virtual bool HasInvalidInput() const { return m_invalidInput; }
   virtual void SetInputValidation(StringValidation::Validator inputValidator, void *data = NULL);
 
 protected:
+  virtual void SetFocus(bool focus);
   virtual void ProcessText(unsigned int currentTime);
+  virtual void RenderText();
   virtual CGUILabel::COLOR GetTextColor() const;
-  CStdStringW GetDisplayedText() const;
+  std::wstring GetDisplayedText() const;
+  std::string GetDescriptionByIndex(int index) const;
+  bool SetStyledText(const std::wstring &text);
   void RecalcLabelPosition();
   void ValidateCursor();
   void UpdateText(bool sendUpdate = true);
   void OnPasteClipboard();
+  void OnSMSCharacter(unsigned int key);
   void DefaultConstructor();
 
-  virtual bool ValidateInput(const CStdStringW &data) const;
+  virtual bool ValidateInput(const std::wstring &data) const;
   void ValidateInput();
 
   /*! \brief Clear out the current text input if it's an MD5 password.
    \return true if the password is cleared, false otherwise.
    */
   bool ClearMD5();
-  
-  CStdStringW m_text2;
-  CStdString  m_text;
+
+  std::wstring m_text2;
+  std::string  m_text;
   CGUIInfoLabel m_hintInfo;
   float m_textOffset;
   float m_textWidth;
+  CRect m_clipRect; ///< clipping rect for the second label
 
   static const int spaceWidth = 5;
 
   unsigned int m_cursorPos;
   unsigned int m_cursorBlink;
 
-  int m_inputHeading;
+  std::string m_inputHeading;
   INPUT_TYPE m_inputType;
   bool m_isMD5;
-  
+
   CGUIAction m_textChangeActions;
 
   bool m_invalidInput;
   StringValidation::Validator m_inputValidator;
   void *m_inputValidatorData;
 
+  unsigned int m_smsKeyIndex;
+  unsigned int m_smsLastKey;
   CStopWatch   m_smsTimer;
 
+  std::wstring m_edit;
+  int          m_editOffset;
+  int          m_editLength;
+
+  static const char*        smsLetters[10];
   static const unsigned int smsDelay;
 };
 #endif
