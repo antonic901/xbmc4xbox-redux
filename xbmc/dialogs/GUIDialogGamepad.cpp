0de78a836907fb02e77f3ce61ff33c342efbc320
antonic901 <antonicnikola6@gmail.com>
2024-06-06 08:40:57 +0200

    [CGUIDialogGamepad] updated to match Kodi Krypton

--- xbmc/dialogs/GUIDialogGamepad.cpp
+++ xbmc/dialogs/GUIDialogGamepad.cpp
@@ -18,23 +18,26 @@
  *
  */
 
-#include "dialogs/GUIDialogGamepad.h"
+#include "GUIDialogGamepad.h"
 #include "utils/md5.h"
-#include "xbox/XKGeneral.h"
-#include "GUIAudioManager.h"
-#include "GUIWindowManager.h"
-#include "dialogs/GUIDialogOK.h"
 #include "utils/StringUtils.h"
-#include "LocalizeStrings.h"
+#include "guilib/GUIAudioManager.h"
+#include "guilib/GUIWindowManager.h"
+#include "GUIDialogOK.h"
+#include "guilib/Key.h"
+#include "guilib/LocalizeStrings.h"
+#include "utils/Variant.h"
+
+#include <utility>
 
 CGUIDialogGamepad::CGUIDialogGamepad(void)
     : CGUIDialogBoxBase(WINDOW_DIALOG_GAMEPAD, "DialogConfirm.xml")
 {
   m_bCanceled = false;
-  m_strUserInput = "";
-  m_strPassword = "";
   m_iRetries = 0;
   m_bUserInputCleanup = true;
+  m_bHideInputChars = true;
+  m_cHideInputChar = '*';
 }
 
 CGUIDialogGamepad::~CGUIDialogGamepad(void)
@@ -55,7 +58,11 @@ bool CGUIDialogGamepad::OnAction(const CAction &action)
   if ((action.GetButtonCode() >= KEY_BUTTON_A &&
        action.GetButtonCode() <= KEY_BUTTON_RIGHT_TRIGGER) ||
       (action.GetButtonCode() >= KEY_BUTTON_DPAD_UP &&
-       action.GetButtonCode() <= KEY_BUTTON_DPAD_RIGHT))
+       action.GetButtonCode() <= KEY_BUTTON_DPAD_RIGHT) ||
+      (action.GetID() >= ACTION_MOVE_LEFT &&
+       action.GetID() <= ACTION_MOVE_DOWN) ||
+      action.GetID() == ACTION_PLAYER_PLAY
+     )
   {
     switch (action.GetButtonCode())
     {
@@ -71,10 +78,21 @@ bool CGUIDialogGamepad::OnAction(const CAction &action)
     case KEY_BUTTON_DPAD_DOWN : m_strUserInput += "D"; break;
     case KEY_BUTTON_DPAD_LEFT : m_strUserInput += "L"; break;
     case KEY_BUTTON_DPAD_RIGHT : m_strUserInput += "R"; break;
-    default : return true; break;
+    default:
+      switch (action.GetID())
+      {
+        case ACTION_MOVE_LEFT:   m_strUserInput += "L"; break;
+        case ACTION_MOVE_RIGHT:  m_strUserInput += "R"; break;
+        case ACTION_MOVE_UP:     m_strUserInput += "U"; break;
+        case ACTION_MOVE_DOWN:   m_strUserInput += "D"; break;
+        case ACTION_PLAYER_PLAY: m_strUserInput += "P"; break;
+        default:
+          return true;
+      }
+      break;
     }
 
-    CStdString strHiddenInput(m_strUserInput);
+    std::string strHiddenInput(m_strUserInput);
     for (int i = 0; i < (int)strHiddenInput.size(); i++)
     {
       strHiddenInput[i] = m_cHideInputChar;
@@ -91,17 +109,14 @@ bool CGUIDialogGamepad::OnAction(const CAction &action)
     Close();
     return true;
   }
-  else if (action.GetButtonCode() == KEY_BUTTON_START)
+  else if (action.GetButtonCode() == KEY_BUTTON_START || action.GetID() == ACTION_SELECT_ITEM)
   {
     m_bConfirmed = false;
     m_bCanceled = false;
 
-    CStdString md5pword2;
-    XBMC::XBMC_MD5 md5state;
-    md5state.append(m_strUserInput);
-    md5state.getDigest(md5pword2);
+    std::string md5pword2 = XBMC::XBMC_MD5::GetMD5(m_strUserInput);
 
-    if (!m_strPassword.Equals(md5pword2))
+    if (!StringUtils::EqualsNoCase(m_strPassword, md5pword2))
     {
       // incorrect password entered
       m_iRetries--;
@@ -163,18 +178,18 @@ bool CGUIDialogGamepad::OnMessage(CGUIMessage& message)
 // \param aTextString String to preload into the keyboard accumulator. Overwritten with user input if return=true.
 // \param dlgHeading String shown on dialog title. Converts to localized string if contains a positive integer.
 // \param bHideUserInput Masks user input as asterisks if set as true.  Currently not yet implemented.
-// \return true if successful display and user input. false if unsucessful display, no user input, or canceled editing.
-bool CGUIDialogGamepad::ShowAndGetInput(CStdString& aTextString, const CStdString &dlgHeading, bool bHideUserInput)
+// \return true if successful display and user input. false if unsuccessful display, no user input, or canceled editing.
+bool CGUIDialogGamepad::ShowAndGetInput(std::string& aTextString, const std::string &dlgHeading, bool bHideUserInput)
 {
   // Prompt user for input
-  CStdString strUserInput = "";
+  std::string strUserInput;
   if (ShowAndVerifyInput(strUserInput, dlgHeading, aTextString, "", "", true, bHideUserInput))
   {
     // user entry was blank
     return false;
   }
 
-  if (strUserInput.IsEmpty())
+  if (strUserInput.empty())
     // user canceled out
     return false;
 
@@ -186,27 +201,27 @@ bool CGUIDialogGamepad::ShowAndGetInput(CStdString& aTextString, const CStdStrin
 
 // \brief Show gamepad keypad twice to get and confirm a user-entered password string.
 // \param strNewPassword String to preload into the keyboard accumulator. Overwritten with user input if return=true.
-// \return true if successful display and user input entry/re-entry. false if unsucessful display, no user input, or canceled editing.
-bool CGUIDialogGamepad::ShowAndVerifyNewPassword(CStdString& strNewPassword)
+// \return true if successful display and user input entry/re-entry. false if unsuccessful display, no user input, or canceled editing.
+bool CGUIDialogGamepad::ShowAndVerifyNewPassword(std::string& strNewPassword)
 {
   // Prompt user for password input
-  CStdString strUserInput = "";
+  std::string strUserInput;
   if (ShowAndVerifyInput(strUserInput, "12340", "12330", "12331", "", true, true))
   {
-    // TODO: Show error to user saying the password entry was blank
-    CGUIDialogOK::ShowAndGetInput(12357, 12358, 0, 0); // Password is empty/blank
+    //! @todo Show error to user saying the password entry was blank
+    CGUIDialogOK::ShowAndGetInput(12357, 12358); // Password is empty/blank
     return false;
   }
 
-  if (strUserInput.IsEmpty())
+  if (strUserInput.empty())
     // user canceled out
     return false;
 
   // Prompt again for password input, this time sending previous input as the password to verify
   if (!ShowAndVerifyInput(strUserInput, "12341", "12330", "12331", "", false, true))
   {
-    // TODO: Show error to user saying the password re-entry failed
-    CGUIDialogOK::ShowAndGetInput(12357, 12344, 0, 0); // Password do not match
+    //! @todo Show error to user saying the password re-entry failed
+    CGUIDialogOK::ShowAndGetInput(12357, 12344); // Password do not match
     return false;
   }
 
@@ -219,25 +234,25 @@ bool CGUIDialogGamepad::ShowAndVerifyNewPassword(CStdString& strNewPassword)
 // \param strPassword Value to compare against user input.
 // \param dlgHeading String shown on dialog title. Converts to localized string if contains a positive integer.
 // \param iRetries If greater than 0, shows "Incorrect password, %d retries left" on dialog line 2, else line 2 is blank.
-// \return 0 if successful display and user input. 1 if unsucessful input. -1 if no user input or canceled editing.
-int CGUIDialogGamepad::ShowAndVerifyPassword(CStdString& strPassword, const CStdString& dlgHeading, int iRetries)
+// \return 0 if successful display and user input. 1 if unsuccessful input. -1 if no user input or canceled editing.
+int CGUIDialogGamepad::ShowAndVerifyPassword(std::string& strPassword, const std::string& dlgHeading, int iRetries)
 {
-  CStdString strLine2 = "";
+  std::string strLine2;
   if (0 < iRetries)
   {
     // Show a string telling user they have iRetries retries left
-    strLine2.Format("%s %i %s", g_localizeStrings.Get(12342).c_str(), iRetries, g_localizeStrings.Get(12343).c_str());
+    strLine2 = StringUtils::Format("%s %i %s", g_localizeStrings.Get(12342).c_str(), iRetries, g_localizeStrings.Get(12343).c_str());
   }
 
   // make a copy of strPassword to prevent from overwriting it later
-  CStdString strPassTemp = strPassword;
+  std::string strPassTemp = strPassword;
   if (ShowAndVerifyInput(strPassTemp, dlgHeading, g_localizeStrings.Get(12330), g_localizeStrings.Get(12331), strLine2, true, true))
   {
     // user entered correct password
     return 0;
   }
 
-  if (strPassTemp.IsEmpty())
+  if (strPassTemp.empty())
     // user canceled out
     return -1;
 
@@ -253,10 +268,10 @@ int CGUIDialogGamepad::ShowAndVerifyPassword(CStdString& strPassword, const CStd
 // \param dlgLine2 String shown on dialog line 2. Converts to localized string if contains a positive integer.
 // \param bGetUserInput If set as true and return=true, strToVerify is overwritten with user input string.
 // \param bHideInputChars Masks user input as asterisks if set as true.  Currently not yet implemented.
-// \return true if successful display and user input. false if unsucessful display, no user input, or canceled editing.
-bool CGUIDialogGamepad::ShowAndVerifyInput(CStdString& strToVerify, const CStdString& dlgHeading,
-    const CStdString& dlgLine0, const CStdString& dlgLine1,
-    const CStdString& dlgLine2, bool bGetUserInput, bool bHideInputChars)
+// \return true if successful display and user input. false if unsuccessful display, no user input, or canceled editing.
+bool CGUIDialogGamepad::ShowAndVerifyInput(std::string& strToVerify, const std::string& dlgHeading,
+    const std::string& dlgLine0, const std::string& dlgLine1,
+    const std::string& dlgLine2, bool bGetUserInput, bool bHideInputChars)
 {
   // Prompt user for password input
   CGUIDialogGamepad *pDialog = (CGUIDialogGamepad *)g_windowManager.GetWindow(WINDOW_DIALOG_GAMEPAD);
@@ -266,24 +281,24 @@ bool CGUIDialogGamepad::ShowAndVerifyInput(CStdString& strToVerify, const CStdSt
 
   // HACK: This won't work if the label specified is actually a positive numeric value, but that's very unlikely
   if (!StringUtils::IsNaturalNumber(dlgHeading))
-    pDialog->SetHeading( dlgHeading );
+    pDialog->SetHeading(dlgHeading);
   else
-    pDialog->SetHeading( atoi(dlgHeading.c_str()) );
+    pDialog->SetHeading(atoi(dlgHeading.c_str()));
 
   if (!StringUtils::IsNaturalNumber(dlgLine0))
-    pDialog->SetLine( 0, dlgLine0 );
+    pDialog->SetLine(0, dlgLine0);
   else
-    pDialog->SetLine( 0, atoi(dlgLine0.c_str()) );
+    pDialog->SetLine(0, atoi(dlgLine0.c_str()));
 
   if (!StringUtils::IsNaturalNumber(dlgLine1))
-    pDialog->SetLine( 1, dlgLine1 );
+    pDialog->SetLine(1, dlgLine1);
   else
-    pDialog->SetLine( 1, atoi(dlgLine1.c_str()) );
+    pDialog->SetLine(1, atoi(dlgLine1.c_str()));
 
   if (!StringUtils::IsNaturalNumber(dlgLine2))
-    pDialog->SetLine( 2, dlgLine2 );
+    pDialog->SetLine(2, dlgLine2);
   else
-    pDialog->SetLine( 2, atoi(dlgLine2.c_str()) );
+    pDialog->SetLine(2, atoi(dlgLine2.c_str()));
 
   g_audioManager.Enable(false); // dont do sounds during pwd input
   pDialog->Open();
@@ -291,10 +306,8 @@ bool CGUIDialogGamepad::ShowAndVerifyInput(CStdString& strToVerify, const CStdSt
 
   if (bGetUserInput && !pDialog->IsCanceled())
   {
-    XBMC::XBMC_MD5 md5state;
-    md5state.append(pDialog->m_strUserInput);
-    md5state.getDigest(strToVerify);
-    strToVerify.ToLower();
+    strToVerify = XBMC::XBMC_MD5::GetMD5(pDialog->m_strUserInput);
+    StringUtils::ToLower(strToVerify);
     pDialog->m_strUserInput = "";
   }
 
