ba9b7dd2c5a515c2318cd14001969cf7408ffd14
antonic901 <antonicnikola6@gmail.com>
2024-07-15 13:46:56 +0200

    [c71a87a] [addons] Allow specifying separate URL for add-on art
    Needed so we do not overload the mirror redirector with SSL requests
    (when datadir is set to an HTTPS URL)

--- xbmc/addons/AddonManager.h
+++ xbmc/addons/AddonManager.h
@@ -249,8 +249,8 @@ namespace ADDON
     bool IsCompatible(const IAddon& addon);
 
     static AddonPtr Factory(const cp_plugin_info_t* plugin, TYPE type);
-    static bool Factory(const cp_plugin_info_t* plugin, TYPE type, CAddonBuilder& builder, bool ignoreExtensions = false);
-    static void FillCpluffMetadata(const cp_plugin_info_t* plugin, CAddonBuilder& builder);
+    static bool Factory(const cp_plugin_info_t* plugin, TYPE type, CAddonBuilder& builder, bool ignoreExtensions = false, std::string assetBasePath = "");
+    static void FillCpluffMetadata(const cp_plugin_info_t* plugin, CAddonBuilder& builder, std::string const& assetBasePath);
 
   private:
     void OnEventSubmit(const std::string& id, const CDateTime& time);
