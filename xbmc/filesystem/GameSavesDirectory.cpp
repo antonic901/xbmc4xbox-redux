aecf4b28ba4bc6ef6779a924391fcfe2bfbecf99
antonic901 <antonicnikola6@gmail.com>
2025-10-23 22:50:15 +0200

    added: dummy implementation for gamesaves listing

---
+++ xbmc/filesystem/GameSavesDirectory.cpp
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (C) 2025-2025 Team XBMC
+ *  This file is part of XBMC - https://github.com/antonic901/xbmc4xbox-redux
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
+ */
+
+#include "GameSavesDirectory.h"
+
+#include "URL.h"
+
+using namespace XFILE;
+
+CGameSavesDirectory::CGameSavesDirectory(void)
+{
+}
+
+CGameSavesDirectory::~CGameSavesDirectory(void)
+{
+}
+
+bool CGameSavesDirectory::GetDirectory(const CURL& url, CFileItemList &items)
+{
+  return false;
+}
