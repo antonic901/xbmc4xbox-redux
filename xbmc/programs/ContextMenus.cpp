61384d4e22fe2ff5d5a88de1544784f7a345cdba
antonic901 <antonicnikola6@gmail.com>
2025-11-10 19:14:42 +0100

    [Context Menu] added: use CProgramInfoTag to determinate if there is additinal information

--- xbmc/programs/ContextMenus.cpp
+++ xbmc/programs/ContextMenus.cpp
@@ -16,6 +16,7 @@
 #include "filesystem/File.h"
 #include "guilib/GUIWindowManager.h"
 #include "interfaces/generic/ScriptInvocationManager.h"
+#include "programs/ProgramDatabase.h"
 #include "programs/dialogs/GUIDialogProgramInfo.h"
 #include "programs/dialogs/GUIDialogProgramSettings.h"
 #include "settings/AdvancedSettings.h"
@@ -37,63 +38,11 @@ bool CProgramInfoBase::IsVisible(const CFileItem& item) const
   if (item.m_bIsFolder)
     return false;
 
-  std::string strNFO = URIUtils::GetParentPath(item.GetPath());
-  strNFO = URIUtils::AddFileToFolder(strNFO, "_resources", "default.xml");
-  if (!XFILE::CFile::Exists(strNFO))
-    return false;
-
-  return URIUtils::HasExtension(item.GetPath(), g_advancedSettings.m_programExtensions);
+  return item.HasProgramInfoTag();
 }
 
 bool CProgramInfoBase::Execute(const boost::shared_ptr<CFileItem>& item) const
 {
-  std::string strRootPath = URIUtils::GetParentPath(item->GetPath());
-  std::string strTempPath = URIUtils::AddFileToFolder(strRootPath, "_resources", "default.xml");
-  CXBMCTinyXML doc;
-  if (doc.LoadFile(strTempPath) && doc.RootElement())
-  {
-    const TiXmlElement* synopsis = doc.RootElement();
-    std::string value;
-    float fValue;
-    int iValue;
-
-    if (XMLUtils::GetString(synopsis, "developer", value))
-      item->SetProperty("developer", value);
-
-    if (XMLUtils::GetString(synopsis, "publisher", value))
-      item->SetProperty("publisher", value);
-
-    if (XMLUtils::GetString(synopsis, "features_general", value))
-      item->SetProperty("features_general", value);
-
-    if (XMLUtils::GetString(synopsis, "features_online", value))
-      item->SetProperty("features_online", value);
-
-    if (XMLUtils::GetString(synopsis, "esrb", value))
-      item->SetProperty("esrb", value);
-
-    if (XMLUtils::GetString(synopsis, "esrb_descriptors", value))
-      item->SetProperty("esrb_descriptors", value);
-
-    if (XMLUtils::GetString(synopsis, "genre", value))
-      item->SetProperty("genre", value);
-
-    if (XMLUtils::GetString(synopsis, "release_date", value))
-      item->SetProperty("release_date", value);
-
-    if (XMLUtils::GetInt(synopsis, "year", iValue))
-      item->SetProperty("year", iValue);
-
-    if (XMLUtils::GetFloat(synopsis, "rating", fValue))
-      item->SetProperty("rating", fValue);
-
-    if (XMLUtils::GetString(synopsis, "platform", value))
-      item->SetProperty("platform", value);
-
-    if (XMLUtils::GetString(synopsis, "exclusive", value))
-      item->SetProperty("exclusive", value);
-  }
-
   CGUIDialogProgramInfo *dialog = static_cast<CGUIDialogProgramInfo*>(g_windowManager.GetWindow(WINDOW_DIALOG_PROGRAM_INFO));
   dialog->SetProgram(item.get());
   dialog->Open();
