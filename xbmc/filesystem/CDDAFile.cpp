antonic901/xbmc4xbox-redux

commit 6a7c9a09505182675345619201824f599d0f89c6

[16b1cba] [vfs] IFile::Read and all derived classes: change to use size_t/ssize_t, do not use more than SSIZE_MAX of buffer for "Read()", small related fixes

xbmc/filesystem/CDDAFile.cpp
@@ -103,15 +103,18 @@ int CCDDAFile::Stat(const CURL& url, struct __stat64* buffer)
   return -1;
 }
 
-unsigned int CCDDAFile::Read(void* lpBuf, int64_t uiBufSize)
+ssize_t CCDDAFile::Read(void* lpBuf, size_t uiBufSize)
 {
   if (!m_pCdIo || !CDetectDVDMedia::IsDiscInDrive())
-    return 0;
+    return -1;
+
+  if (uiBufSize > SSIZE_MAX)
+    uiBufSize = SSIZE_MAX;
 
   int iSectorCount = (int)uiBufSize / CDIO_CD_FRAMESIZE_RAW;
 
   if (iSectorCount <= 0)
-    return 0;
+    return -1;
 
   // Are there enough sectors left to read
   if (m_lsnCurrent + iSectorCount > m_lsnEnd)
@@ -120,7 +123,7 @@ unsigned int CCDDAFile::Read(void* lpBuf, int64_t uiBufSize)
   if (m_cdio->cdio_read_audio_sectors(m_pCdIo, lpBuf, m_lsnCurrent, iSectorCount) != DRIVER_OP_SUCCESS)
   {
     CLog::Log(LOGERROR, "file cdda: Reading %d sectors of audio data starting at lsn %d failed", iSectorCount, m_lsnCurrent);
-    return 0;
+    return -1;
   }
 
   m_lsnCurrent += iSectorCount;

xbmc/filesystem/CDDAFile.h
@@ -33,7 +33,7 @@ public:
   virtual bool Exists(const CURL& url);
   virtual int Stat(const CURL& url, struct __stat64* buffer);
 
-  virtual unsigned int Read(void* lpBuf, int64_t uiBufSize);
+  virtual ssize_t Read(void* lpBuf, size_t uiBufSize);
   virtual int64_t Seek(int64_t iFilePosition, int iWhence = SEEK_SET);
   virtual void Close();
   virtual int64_t GetPosition();

xbmc/filesystem/CurlFile.cpp
@@ -1380,7 +1380,7 @@ int CCurlFile::Stat(const CURL& url, struct __stat64* buffer)
   return 0;
 }
 
-unsigned int CCurlFile::CReadState::Read(void* lpBuf, int64_t uiBufSize)
+unsigned int CCurlFile::CReadState::Read(void* lpBuf, size_t uiBufSize)
 {
   /* only request 1 byte, for truncated reads (only if not eof) */
   if((m_fileSize == 0 || m_filePos < m_fileSize) && !FillBuffer(1))
@@ -1400,7 +1400,7 @@ unsigned int CCurlFile::CReadState::Read(void* lpBuf, int64_t uiBufSize)
   if (!m_stillRunning && (m_fileSize == 0 || m_filePos != m_fileSize))
   {
     CLog::Log(LOGWARNING, "%s - Transfer ended before entire file was retrieved pos %"PRId64", size %"PRId64, __FUNCTION__, m_filePos, m_fileSize);
-    return 0;
+    return -1;
   }
 
   return 0;

xbmc/filesystem/CurlFile.h
@@ -58,7 +58,7 @@ namespace XFILE
       virtual int	Stat(const CURL& url, struct __stat64* buffer);
 	    virtual void Close();
       virtual bool ReadString(char *szLine, int iLineLength)     { return m_state->ReadString(szLine, iLineLength); }
-      virtual unsigned int Read(void* lpBuf, int64_t uiBufSize)  { return m_state->Read(lpBuf, uiBufSize); }
+      virtual ssize_t Read(void* lpBuf, size_t uiBufSize)        { return m_state->Read(lpBuf, uiBufSize); }
       virtual int Write(const void* lpBuf, int64_t uiBufSize);
       virtual CStdString GetMimeType()                           { return m_state->m_httpheader.GetMimeType(); }
       virtual int IoControl(EIoControl request, void* param);
@@ -130,7 +130,7 @@ namespace XFILE
           size_t HeaderCallback(void *ptr, size_t size, size_t nmemb);
 
           bool         Seek(int64_t pos);
-          unsigned int Read(void* lpBuf, int64_t uiBufSize);
+          unsigned int Read(void* lpBuf, size_t uiBufSize);
           bool         ReadString(char *szLine, int iLineLength);
           bool         FillBuffer(unsigned int want);
           void         SetReadBuffer(const void* lpBuf, int64_t uiBufSize);

xbmc/filesystem/DAAPFile.cpp
@@ -198,7 +198,7 @@ bool CDAAPFile::Open(const CURL& url)
 
 
 //*********************************************************************************************
-unsigned int CDAAPFile::Read(void *lpBuf, int64_t uiBufSize)
+ssize_t CDAAPFile::Read(void *lpBuf, size_t uiBufSize)
 {
   return m_curl.Read(lpBuf, uiBufSize);
 }

xbmc/filesystem/DAAPFile.h
@@ -72,7 +72,7 @@ public:
   virtual bool Open(const CURL& url);
   virtual bool Exists(const CURL& url);
   virtual int Stat(const CURL& url, struct __stat64* buffer);
-  virtual unsigned int Read(void* lpBuf, int64_t uiBufSize);
+  virtual ssize_t Read(void* lpBuf, size_t uiBufSize);
   virtual int64_t Seek(int64_t iFilePosition, int iWhence = SEEK_SET);
   virtual void Close();
   virtual int  IoControl(EIoControl request, void* param);

xbmc/filesystem/File.cpp
@@ -137,10 +137,10 @@ bool CFile::Copy(const CURL& url2, const CURL& dest, XFILE::IFileCallback* pCall
     }
 
     // 128k is optimal for xbox
-    int iBufferSize = 128 * 1024;
+    static const int iBufferSize = 128 * 1024;
 
     CAutoBuffer buffer(iBufferSize);
-    int iRead, iWrite;
+    ssize_t iRead, iWrite;
 
     UINT64 llFileSize = file.GetLength();
     UINT64 llPos = 0;
@@ -495,6 +495,9 @@ unsigned int CFile::Read(void *lpBuf, int64_t uiBufSize)
 {
   if (!m_pFile || !lpBuf)
     return 0;
+
+  if (uiBufSize > SSIZE_MAX)
+    uiBufSize = SSIZE_MAX;
 
   if(m_pBuffer)
   {

xbmc/filesystem/FileCache.cpp
@@ -349,13 +349,13 @@ int CFileCache::Stat(const CURL& url, struct __stat64* buffer)
   return CFile::Stat(url.Get(), buffer);
 }
 
-unsigned int CFileCache::Read(void* lpBuf, int64_t uiBufSize)
+ssize_t CFileCache::Read(void* lpBuf, size_t uiBufSize)
 {
   CSingleLock lock(m_sync);
   if (!m_pCache)
   {
     CLog::Log(LOGERROR,"%s - sanity failed. no cache strategy!", __FUNCTION__);
-    return 0;
+    return -1;
   }
   int64_t iRc;
 
@@ -379,7 +379,7 @@ retry:
   if (iRc == CACHE_RC_TIMEOUT)
   {
     CLog::Log(LOGWARNING, "%s - timeout waiting for data", __FUNCTION__);
-    return 0;
+    return -1;
   }
 
   if (iRc == 0)
@@ -387,7 +387,7 @@ retry:
 
   // unknown error code
   CLog::Log(LOGERROR, "%s - cache strategy returned unknown error code %d", __FUNCTION__, (int)iRc);
-  return 0;
+  return -1;
 }
 
 int64_t CFileCache::Seek(int64_t iFilePosition, int iWhence)

commit db9f8b5ddf2a9059b2abbf43357c888c4711c1f9

fixed: ripping Audio-CD was broken after https://github.com/antonic901/xbmc4xbox-redux/commit/6a7c9a09505182675345619201824f599d0f89c6

xbmc/filesystem/CDDAFile.cpp
@@ -129,7 +129,7 @@ ssize_t CFileCDDA::Read(void* lpBuf, size_t uiBufSize)
   int iSectorCount = std::min((int)uiBufSize / CDIO_CD_FRAMESIZE_RAW, m_iSectorCount);
 
   if (iSectorCount <= 0)
-    return -1;
+    return 0;
 
   // Are there enough sectors left to read
   if (m_lsnCurrent + iSectorCount > m_lsnEnd)