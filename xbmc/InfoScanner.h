31840109e3ffb3728a5672262c5b26f3052aa8e7
antonic901 <antonicnikola6@gmail.com>
2026-05-29 13:22:45 +0200

    [CInfoScanner] update to match Kodi Omega

--- xbmc/InfoScanner.h
+++ xbmc/InfoScanner.h
@@ -1,22 +1,11 @@
 /*
- *      Copyright (C) 2016 Team Kodi
- *      http://kodi.tv
+ *  Copyright (C) 2016-2018 Team Kodi
+ *  This file is part of Kodi - https://kodi.tv
  *
- *  This Program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2, or (at your option)
- *  any later version.
- *
- *  This Program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with KODI; see the file COPYING.  If not, see
- *  <http://www.gnu.org/licenses/>.
- *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSES/README.md for more information.
  */
+
 #pragma once
 
 #include <set>
@@ -41,17 +30,31 @@ public:
     INFO_ADDED
   };
 
+  /*
+   \brief Type of information returned from tag readers.
+  */
+
+  enum INFO_TYPE
+  {
+    NO_NFO       = 0, //!< No info found
+    FULL_NFO     = 1, //!< Full info specified
+    URL_NFO      = 2, //!< A URL to grab info from was found
+    OVERRIDE_NFO = 3, //!< Override info was found
+    COMBINED_NFO = 4, //!< A URL to grab info from + override info was found
+    ERROR_NFO    = 5, //!< Error processing info
+    TITLE_NFO    = 6  //!< At least Title was read (and optionally the Year)
+  };
+
   //! \brief Empty destructor.
-  virtual ~CInfoScanner() { };
+  virtual ~CInfoScanner() {}
 
   virtual bool DoScan(const std::string& strDirectory) = 0;
 
   /*! \brief Check if the folder is excluded from scanning process
    \param strDirectory Directory to scan
-   \param regexps Regular expression to exclude from the scan
-   \return true if there is a .nomedia file or one of the regexps is a match
+   \return true if there is a .nomedia file
    */
-  bool IsExcluded(const std::string& strDirectory, const std::vector<std::string> &regexps);
+  bool HasNoMedia(const std::string& strDirectory) const;
 
   //! \brief Set whether or not to show a progress dialog.
   void ShowDialog(bool show) { m_showDialog = show; }
@@ -61,21 +64,12 @@ public:
 
 protected:
   //! \brief Protected constructor to only allow subclass instances.
-  CInfoScanner()
-    : m_showDialog(false)
-    , m_handle(NULL)
-    , m_bRunning(false)
-    , m_bCanInterrupt(false)
-    , m_bClean(false)
-  { };
+  CInfoScanner() : m_showDialog(false), m_handle(NULL), m_bRunning(false), m_bCanInterrupt(false), m_bClean(false) {}
 
   std::set<std::string> m_pathsToScan; //!< Set of paths to scan
   bool m_showDialog; //!< Whether or not to show progress bar dialog
   CGUIDialogProgressBarHandle* m_handle; //!< Progress bar handle
   bool m_bRunning; //!< Whether or not scanner is running
-  bool m_bCanInterrupt; //!< Whether or not scanner is currently interruptable
+  bool m_bCanInterrupt; //!< Whether or not scanner is currently interruptible
   bool m_bClean; //!< Whether or not to perform cleaning during scanning
-
-private:
-  bool HasNoMedia(const std::string& strDirectory) const;
 };
