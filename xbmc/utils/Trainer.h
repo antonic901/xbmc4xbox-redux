21560d91dfe28bf858d635309dbaec04793ba048
illusiony <37698908+illusionyy@users.noreply.github.com>
2026-09-16 07:43:34 +0100

    free trainer heap if using ptm extension

--- xbmc/utils/Trainer.h
+++ xbmc/utils/Trainer.h
@@ -35,6 +35,7 @@ public:
   static bool RemoveTrainer();
 
   bool Load(const std::string& strPath);
+  void FreeData();
   void GetTitleIds(unsigned int& title1, unsigned int& title2, unsigned int& title3) const;
   void GetOptionLabels(std::vector<std::string>& vecOptionLabels) const;
 
@@ -47,6 +48,7 @@ public:
   inline const int GetTrainerId() { return m_idTrainer; }
 
   inline bool IsXBTF() const { return m_bIsXBTF; }
+  inline bool IsPTM() const { return m_bIsPTM; }
   inline int Size() const { return m_iSize; }
 
   inline const unsigned char* data() const { return m_pTrainerData; }
@@ -65,6 +67,7 @@ protected:
   unsigned int m_iOptions;
   int m_idTrainer;
   bool m_bIsXBTF;
+  bool m_bIsPTM;
   char m_szCreationKey[200];
   std::vector<std::string> m_vecText;
   std::string m_strPath;
